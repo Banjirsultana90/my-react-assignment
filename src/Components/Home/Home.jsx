8dedc7ea48dbc7987bc4b1ae7f4ca73918e7113b
Banjirsultana90 <banjirsultana15@gmail.com>
2023-09-15 00:02:29 +0600

    Basic structure for starting project completed

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './home.css'
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="card-container">
+                <div className="cards">This is card
+
+                </div>
+                <div className="cart">This is cart</div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Home;
